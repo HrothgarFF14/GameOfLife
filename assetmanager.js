7513b470f155e747558efefbe18ffbbcba5512a8
Louis Lomboy <124099680+HrothgarFF14@users.noreply.github.com>
2024-04-07 00:31:30 -0700

    Automata test

--- assetmanager.js
+++ assetmanager.js
@@ -1,20 +1,37 @@
 class AssetManager {
+    /**
+     * Constructs the AssetManager class
+     */
     constructor() {
         this.successCount = 0;
         this.errorCount = 0;
         this.cache = [];
         this.downloadQueue = [];
-    };
+    }
 
+    /**
+     * Add asset's path to download queue
+     * @param {String} path
+     */
     queueDownload(path) {
         console.log("Queueing " + path);
         this.downloadQueue.push(path);
-    };
+    }
 
+    /**
+     * Check if queued assets have finished
+     * @returns boolean value
+     */
     isDone() {
-        return this.downloadQueue.length === this.successCount + this.errorCount;
-    };
+        return (
+            this.downloadQueue.length === this.successCount + this.errorCount
+        );
+    }
 
+    /**
+     * Start loading all queued assets, executing a callback when all are processed
+     * @param callback
+     */
     downloadAll(callback) {
         if (this.downloadQueue.length === 0) setTimeout(callback, 10);
         for (let i = 0; i < this.downloadQueue.length; i++) {
@@ -38,10 +55,14 @@ class AssetManager {
             img.src = path;
             this.cache[path] = img;
         }
-    };
+    }
 
+    /**
+     * Retrieve loaded asset from the cache by path
+     * @param {String} path
+     * @returns String path
+     */
     getAsset(path) {
         return this.cache[path];
-    };
-};
-
+    }
+}
